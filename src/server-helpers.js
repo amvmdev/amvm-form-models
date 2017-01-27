1cac079aa6b3ccccf1bdd3488a83c4d44ae4c8dc
amvmdev <amvm.dev@gmail.com>
2017-01-27 00:36:30 -0500

    Modified createFullModel and createFormModel to accept function that can modify json object

--- src/server-helpers.js
+++ src/server-helpers.js
@@ -1,10 +1,10 @@
 import FormModelValidator from './form-model-validator';
 
-export function createFullModel(json, fnFormModel, fnFormModelValidators, fnFullModel, httpRequest) {
+export function createFullModel(json, fnFormModel, fnFormModelValidators, fnFullModel, fnPostProcess) {
     /*
     {   
         isValid: true if we can validate json
-        json: json with meta property. This json will go into database
+        json: this json will go into database
         errors: object with errors if validation fails
         formModel: form model created from json
     }
@@ -24,7 +24,7 @@ export function createFullModel(json, fnFormModel, fnFormModelValidators, fnFull
     // save result of validating form model
     result.isValid = FormModelValidator.isModelValid(formModel, new fnFormModelValidators());
     result.formModel = formModel;
-    
+
     if (!result.isValid) {
         // if form model is not valid, then create object with errors for that model.
         // this error object will be send to client to display validation errors
@@ -41,20 +41,11 @@ export function createFullModel(json, fnFormModel, fnFormModelValidators, fnFull
 
         // this json contains all fields for that domain model        
         let fullModelJson = new fnFullModel(jsonFromFormModel);
-         
-        delete fullModelJson.meta; // delete meta in case it exists
-        delete fullModelJson._id; // if we don't delete _id, new record will be created with empty _id
 
-        // Important! - create meta property on json that points to who is owning this data!!!
-        // Also get additional info
-        const ip = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
-        const userAgent = httpRequest.headers['user-agent'];
-        fullModelJson.meta = {
-            ownerId: httpRequest.user._id,
-            createdOn: Date.now(),
-            ip: ip,
-            userAgent: userAgent
-        };
+        // call function that may modify json document
+        if (fnPostProcess && typeof fnPostProcess === 'function') {
+            fnPostProcess(fullModelJson);
+        }
 
         // Finally, after all checks, we return json on full model with meta data added to it            
         result.json = fullModelJson;
@@ -66,74 +57,7 @@ export function createFullModel(json, fnFormModel, fnFormModelValidators, fnFull
 
 
 
-export function createFullModelAnon(json, fnFormModel, fnFormModelValidators, fnFullModel, httpRequest) {
-    /*
-    {   
-        isValid: true if we can validate json
-        json: json with metadata. This json will go into database
-        errors: object with errors if validation fails
-        formModel: form model created from json
-    }
-    */
-
-    let result = {
-        isValid: false,
-        json: null,
-        errors: null,
-        formModel: null
-    };
-
-
-    // create form model from raw data
-    let formModel = new fnFormModel(json);
-
-    // save result of validating form model
-    result.isValid = FormModelValidator.isModelValid(formModel, new fnFormModelValidators());
-    result.formModel = formModel;
-    
-    if (!result.isValid) {
-        // if form model is not valid, then create object with errors for that model.
-        // this error object will be send to client to display validation errors
-        result.errors = FormModelValidator.getErrors(formModel);
-
-        // set flag that model is invalid
-        result.isValid = false;
-    }
-    else {
-        // form model is valid, then create json that will be saved into database (with metadata)
-
-        // get json from form model (this way we get only json that we expect)
-        let jsonFromFormModel = FormModelValidator.getJSON(formModel, new fnFormModelValidators());
-
-        // this json contains all fields for that domain model        
-        let fullModelJson = new fnFullModel(jsonFromFormModel);
-         
-        delete fullModelJson.meta; // delete meta in case it exists
-        delete fullModelJson._id; // if we don't delete _id, new record will be created with empty _id
-
-        // get info about client
-        let ip = '';
-        let userAgent = '';
-        if(httpRequest) {
-            ip = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
-            userAgent = httpRequest.headers['user-agent'];
-        }        
-        fullModelJson.meta = {
-            createdOn: Date.now(),
-            ip: ip,
-            userAgent: userAgent
-        };
-       
-        // Finally, after all checks, we return json on full model with meta data added to it            
-        result.json = fullModelJson;
-        result.isValid = true;
-    }
-
-    return result;
-}
-
-
-export function createFormModel(json, fnFormModel, fnFormModelValidators) {
+export function createFormModel(json, fnFormModel, fnFormModelValidators, fnPostProcess) {
     /*
     {   
         isValid: true if we can validate json
@@ -170,7 +94,12 @@ export function createFormModel(json, fnFormModel, fnFormModelValidators) {
 
         // get json from form model (this way we get only json that we expect)
         let jsonFromFormModel = FormModelValidator.getJSON(formModel, new fnFormModelValidators());
-        
+
+        // call function that may modify json document
+        if (fnPostProcess && typeof fnPostProcess === 'function') {
+            fnPostProcess(jsonFromFormModel);
+        }
+
         // Finally, after all checks, we return json on full model with meta data added to it            
         result.json = jsonFromFormModel;
         result.isValid = true;
